009628f1fbf9d44dfc6b33196dcab639860cc1e5
dimasrmp <dimasramaputra@gmail,com>
2018-06-04 01:01:50 +0700

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,8 @@
     
     <!-- Styles -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    
 </head>
 <body>
 <section id="page-top"></section>
@@ -105,5 +107,11 @@
 </body>
 <script type="text/javascript" src="{{ asset('js/jquery-3.2.0.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/jquery.easing.min.js') }}"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
 <script type="text/javascript" src="{{ asset('js/core.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/try.js') }}"></script>
+
+
 </html>
+
--- resources/views/sections/galeri.blade.php
+++ resources/views/sections/galeri.blade.php
@@ -7,4 +7,74 @@
 			</div>
 		</div>
 	</div>
+<section id="gallery">
+  <div class="container">
+    <div id="image-gallery">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_1.jpg') }}"><img src="{{ asset('img/IMG_1.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_2.jpg') }}"><img src="{{ asset('img/IMG_2.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_3.jpg') }}"><img src="{{ asset('img/IMG_3.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_4.jpg') }}"><img src="{{ asset('img/IMG_4.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_5.jpg') }}"><img src="{{ asset('img/IMG_5.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_7.jpg') }}"><img src="{{ asset('img/IMG_7.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_8.jpg') }}"><img src="{{ asset('img/IMG_8.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/IMG_6.jpg') }}"><img src="{{ asset('img/IMG_6.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+      </div><!-- End row -->
+    </div><!-- End image gallery -->
 </section>
