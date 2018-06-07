d8cc74452ba5a3a68d95de2d326397cf0c5666d2
dimasrmp <dimasramaputra@gmail,com>
2018-06-07 20:18:42 +0700

    j
    ,

--- resources/views/sections/galeri.blade.php
+++ resources/views/sections/galeri.blade.php
@@ -7,10 +7,10 @@
 			</div>
 		</div>
 	</div>
-
+<!-- 
 <body>
-
-  <div class="container">
+ -->
+  <!-- <div class="container">
   <div class="row">
     <div class="col-md-12">
     <div class="portfolioFilter clearfix">
@@ -90,82 +90,135 @@
     </div>
     </div>
   </div>
-</div>
-  </section>
-<!-- <section id="gallery">
-  <div class="container">
+</div> -->
+
+<!--   <div class="container">
   <div id="myBtnContainer">
   <button class="btn-gallery active" onclick="filterSelection('all')"> Show All</button>
   <button class="btn-gallery" onclick="filterSelection('tahun1')"> 2015</button>
   <button class="btn-gallery" onclick="filterSelection('tahun2')"> 2016</button>
 
-</div>
+</div> -->
+<section id="gallery">
     <div id="image-gallery">
+    <div class="container">
       <div class="row">
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun1">
-            <a href="{{ asset('img/IMG_1.jpg') }}"><img src="{{ asset('img/IMG_1.jpg') }}" class="img-responsive"></a>
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2015_1.jpg') }}"><img src="{{ asset('img/2015_1.jpg') }}" class="img-responsive"></a>
             <div class="img-overlay">
               <i class="fa fa-eye" aria-hidden="true"></i>
             </div>
           </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun1">
-            <a href="{{ asset('img/IMG_2.jpg') }}"><img src="{{ asset('img/IMG_2.jpg') }}" class="img-responsive"></a>
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2015_2.jpg') }}"><img src="{{ asset('img/2015_2.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2015_3.jpg') }}"><img src="{{ asset('img/2015_3.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2015_4.jpg') }}"><img src="{{ asset('img/2015_4.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2016_1.jpg') }}"><img src="{{ asset('img/2016_1.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2016_2.jpg') }}"><img src="{{ asset('img/2016_2.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2016_3.jpg') }}"><img src="{{ asset('img/2016_3.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_1.jpg') }}"><img src="{{ asset('img/2017_1.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_2.jpg') }}"><img src="{{ asset('img/2017_2.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_3.jpg') }}"><img src="{{ asset('img/2017_3.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_4.jpg') }}"><img src="{{ asset('img/2017_4.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_5.jpg') }}"><img src="{{ asset('img/2017_5.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_6.jpg') }}"><img src="{{ asset('img/2017_6.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_7.jpg') }}"><img src="{{ asset('img/2017_7.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2017_8.jpg') }}"><img src="{{ asset('img/2017_8.jpg') }}" class="img-responsive"></a>
+            <div class="img-overlay">
+              <i class="fa fa-eye" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div><div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+          <div class="img-wrapper">
+            <a href="{{ asset('img/2015_5.jpg') }}"><img src="{{ asset('img/2015_5.jpg') }}" class="img-responsive"></a>
             <div class="img-overlay">
               <i class="fa fa-eye" aria-hidden="true"></i>
             </div>
           </div>
         </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun1">
-            <a href="{{ asset('img/IMG_3.jpg') }}"><img src="{{ asset('img/IMG_3.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun1">
-            <a href="{{ asset('img/IMG_4.jpg') }}"><img src="{{ asset('img/IMG_4.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun2">
-            <a href="{{ asset('img/IMG_5.jpg') }}"><img src="{{ asset('img/IMG_5.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun2">
-            <a href="{{ asset('img/IMG_7.jpg') }}"><img src="{{ asset('img/IMG_7.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun2">
-            <a href="{{ asset('img/IMG_8.jpg') }}"><img src="{{ asset('img/IMG_8.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-          <div class="img-wrapper tahun2">
-            <a href="{{ asset('img/IMG_6.jpg') }}"><img src="{{ asset('img/IMG_6.jpg') }}" class="img-responsive"></a>
-            <div class="img-overlay">
-              <i class="fa fa-eye" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-      </div><!-- End row -->
-  <!--   </div>End image gallery
-</section> -- -->>
+    </div>
+    </div>
+ </div>
+
+</section>    
